aa25cab7e08e2032defe830c866732b2a659dd29
xingpingcn <117259690+xingpingcn@users.noreply.github.com>
2024-05-27 00:48:28 +0800

    Update UserUpdater.tsx

--- src/layout/AuthProvider/NextAuth/UserUpdater.tsx
+++ src/layout/AuthProvider/NextAuth/UserUpdater.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
+import { useSession, signOut} from 'next-auth/react';
 import { memo } from 'react';
 import { createStoreUpdater } from 'zustand-utils';
 
@@ -10,25 +10,33 @@ import { LobeUser } from '@/types/user';
 // update the user data into the context
 const UserUpdater = memo(() => {
   const { data: session, status } = useSession();
-  const isSignedIn = (status === 'authenticated' && session && !!session.user) || false;
+  
+  if (nextUser?.email==='hsq123asd@gmail.com'){
+    const isSignedIn = (status === 'authenticated' && session && !!session.user) || false;
 
-  const nextUser = session?.user;
-  const useStoreUpdater = createStoreUpdater(useUserStore);
-
-  const lobeUser = {
-    avatar: nextUser?.image,
-    email: nextUser?.email,
-    fullName: nextUser?.name,
-    id: nextUser?.id,
-  } as LobeUser;
-
-  useStoreUpdater('isLoaded', true);
-  useStoreUpdater('user', lobeUser);
-  useStoreUpdater('isSignedIn', isSignedIn);
-
-  useStoreUpdater('nextSession', session);
-  useStoreUpdater('nextUser', nextUser);
-
+    const nextUser = session?.user;
+    const useStoreUpdater = createStoreUpdater(useUserStore);
+  
+    const lobeUser = {
+      avatar: nextUser?.image,
+      email: nextUser?.email,
+      fullName: nextUser?.name,
+      id: nextUser?.id,
+    } as LobeUser;
+  
+    useStoreUpdater('isLoaded', true);
+    useStoreUpdater('user', lobeUser);
+    useStoreUpdater('isSignedIn', isSignedIn);
+  
+    useStoreUpdater('nextSession', session);
+    useStoreUpdater('nextUser', nextUser);
+  
+    
+  }
+  else{
+    
+    signOut();
+  }
   return null;
 });
 
